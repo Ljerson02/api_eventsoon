51a6c3c34979eb7054fef8cdda11d8e85d9e4b0b
jlozano <jlozano@jpglobaldigital.com>
2020-06-06 06:46:42 -0500

    Remove type module

--- start.js
+++
@@ -1,9 +0,0 @@
-// Transpile all code following this line with babel and use '@babel/preset-env' (aka ES6) preset.
-require("@babel/register")({
-  presets: ["@babel/preset-env"],
-  "plugins": [
-    "@babel/plugin-transform-runtime"
-  ]
-});
-require('module-alias/register')
-module.exports = require('./app.js')
